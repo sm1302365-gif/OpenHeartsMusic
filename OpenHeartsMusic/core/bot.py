75b3884c7d3102986e0618af8eddd44d28fedb48
sm1302365-gif <sm1302365@gmail.com>
2026-09-17 19:28:09 +0530

    Update bot.py

--- OpenHeartsMusic/core/bot.py
+++ OpenHeartsMusic/core/bot.py
@@ -38,6 +38,7 @@ class Bot(hydrogram.Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
+            proxy=config.PROXY,
             parse_mode=hydrogram.enums.ParseMode.HTML,
             max_concurrent_transmissions=7,
         )
